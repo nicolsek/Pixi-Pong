b8b257db8cfe607dc9f3e25ae4412e321e4b3908
nicolsek <tusek.nicole@gmail.com>
2017-10-09 15:58:19 -0700

    Added smooth keyboard movement.

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ var stage = new PIXI.Container();
 
 /* Drawing the screen */
 document.body.appendChild(renderer.view);
+
+/* Adding events for handling */
 document.addEventListener('keydown', onKeyDown); //On key down call function
+document.addEventListener('keyup', onKeyUp)
 
 /* Properties of the ball's position */
 var ballX = width/2; //Starting off the ball's position is going to be the center of the screen.
@@ -18,26 +21,60 @@ var ballY = height/2; //Width with my weird width scale thing.
 var ballAngle = (Math.random() * 360) + 1; //Angle of the ball random each time.
 var ballSpeed = 8; //Is one for now.
 
-
 /* Properties of the ball's paddle */
 var paddleX = width/15;
 var paddleY = height/2 - 100/2;
-var paddleSpeed = 40;
+var paddleSpeed = 10;
 
 /* Adding Graphics */
 stage.addChild(graphics); //Add graphics to stage.
 
+var keys = [false, false]; //Keys of W and Up being 0, and S and Down being 1.
+
 /* Animating */
 animate();
 
+/**
+ * @desc Handling the key presses.
+ * @param {object} key the key that is being pressed.
+ */
 function onKeyDown(key) {
     //Up
     if (key.keyCode === 87 || key.keyCode === 38) {
-        paddleY -= paddleSpeed;
+        keys[0] = true;
     }
 
     //Down
     if (key.keyCode === 83 || key.keyCode === 40) {
+        keys[1] = true;
+    }
+}
+
+/**
+ * @desc Handling the key presses.
+ * @param {object} key the key that is being pressed.
+ */
+function onKeyUp(key) {
+    //Up
+    if (key.keyCode === 87 || key.keyCode === 38) {
+        keys[0] = false;
+    }
+
+    //Down
+    if (key.keyCode === 83 || key.keyCode === 40) {
+        keys[1] = false;
+    }    
+}
+
+/**
+ * @desc Handling the logic of if keys are down.
+ */
+function handleKeys() {
+    if (keys[0]) { //While up is pressed.
+        paddleY -= paddleSpeed;
+    }
+
+    if (keys[1]) { //While down is pressed.
         paddleY += paddleSpeed;
     }
 }
@@ -77,6 +114,7 @@ function drawPaddles() {
 function animate() {
     graphics.clear(); //Clear scene
 
+    handleKeys();
     doPhysics();
 
     drawBall();
